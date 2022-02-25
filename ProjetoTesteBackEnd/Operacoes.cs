fc1ad21e6cce85ea6aa3b9aa09869363fa22f8eb
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-25 15:57:59 -0300

    Adicionando Metodo de Teste no Operacoes

---
+++ ProjetoTesteBackEnd/Operacoes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoTesteBackEnd
+{
+    public class Operacoes
+    {
+        public static double Somar(double pNum, double sNum)
+        {
+            return pNum + sNum;
+            //  Pode ser escrito return (pNum + sNum);
+        }
+    }
+}
