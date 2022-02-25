8f7569582d670083126e800e3dea8d9498761495
omartins-zs <gabrielmatheus209@gmail.com>
2022-02-25 16:35:04 -0300

    Adicionando Conjunto de Teste no TesteMSTeste

--- TesteMSTest/TesteMSTeste.cs
+++ TesteMSTest/TesteMSTeste.cs
@@ -19,5 +19,16 @@ namespace TesteMSTest
             // Assert - Verificar 
             Assert.AreEqual(2, resultado);
         }
+
+        [DataTestMethod]
+        [DataRow(1, 1, 2)]
+        [DataRow(1, 1, 3)]
+        [DataRow(1, 2, 3)]
+        public void SomarDoisNumerosListas(double pNum, double sNum, double tNum)
+        {
+            var resultado = Operacoes.Somar(pNum, sNum);
+
+            Assert.AreEqual(tNum, resultado);
+        }
     }
 }
